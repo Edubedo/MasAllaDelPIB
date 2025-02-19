2cb4d350291f6bd6d709c734c21f6e91551a619c
pamela <drodriguez64@col.mx>
2025-02-19 17:09:41 -0600

    Avance de barra de navegacion

--- app/visitante/views/layouts/navbar.php
+++ app/visitante/views/layouts/navbar.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../layouts/css/navbar.css">
+    <title>Navbar></title>
 </head>
 <body>
-    
+    <nav>
+        <input type="checkbox" id="toggle">
+        <div class="logo">
+            <img src="./img/logo.png" alt="imagen logo empresa" width="50" height="50">
+            MAS ALLA DEL PIB
+        </div>
+        <ul class="list">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Inicio de Sesión</a></li>
+            <li><a href="#">Publicaciones</a></li>
+            <li><a href="#">Nosotros</a></li>
+        </ul>
+        <label for="toggle" class="icon-bars">&#9776;
+        </label>
+    </nav>
 </body>
 </html>
