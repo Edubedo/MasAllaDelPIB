994e910495284bd5de9373d94bec690d3b1f8178
eescobedo <ezescobedo27@gmail.com>
2025-02-25 21:32:31 -0600

    Carrousel

--- views/layout/carousel.php
+++ views/layout/carousel.php
@@ -4,26 +4,33 @@
         height: 50vh;
         object-fit: cover;
     }
+
+    .carousel .carousel-inner a {
+        color: #fff;
+        text-decoration: none;
+        background-color: rgba(0, 0, 0, 0.5);
+        padding: 10px 20px;
+        border-radius: 5px;
+        margin-top: 1rem;
+        font-size: 1.5rem;
+    }
+
+    .carousel .carousel-inner a:hover {
+        color: #fff;
+        background-color: rgba(0, 0, 0, 0.7);
+    }
 </style>
 <div class="p-0 mb-5 container-fluid">
     <div id="blog-carousel" class="carousel slide overlay-bottom" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="w-100" src="assets/img/img_carrousel.jpg " alt="Image">
+                <img class="w-100" src="assets/img/img_empleo.jpg " alt="Image">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <h1 class="m-0 display-1 text-primary">
                         Trabajo Colima
                     </h1>
-                    <h2 class="m-0 text-white">Trabajo Colima invita a participar en la expo virtual de Empleo</h2>
+                    <h2 class="m-0 text-white">¡Trabajo Colima te invita a participar en la Expo Virtual de Empleo! Este evento es una excelente oportunidad para interactuar con empresas de diversos sectores en busca de talento.</h2>
                     <a href="#" class="btn btn-primary">Ver más</a>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img class="w-100" src="assets/img/img_carrousel.jpg" alt="Image">
-                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                    <h2 class="m-0 text-white font-weight-medium">Sistema de control</h2>
-                    <h1 class="m-0 display-1 text-primary">CENTRO DE COMPUTO</h1>
-                    <h2 class="m-0 text-white">Universidad de Colima</h2>
                 </div>
             </div>
         </div>
