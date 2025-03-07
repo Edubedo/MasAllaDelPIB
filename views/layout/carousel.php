f74d53cce20c38f8891c2f40c113ff677d16e8af
nanchi1005 <nancy16laureano@gmail.com>
2025-03-07 09:30:07 -0600

    cambios

--- views/layout/carousel.php
+++ views/layout/carousel.php
@@ -5,45 +5,104 @@ require 'config/database.php';
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
-    <!-- Incluyendo los estilos -->
-     <link rel="stylesheet" href="./views/css/carousel.css">
-    
-</head>
+    <head>
+        <!-- Incluyendo los estilos -->
+        <link rel="stylesheet" href="./views/css/carousel.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+        
+    </head>
 
-<body>
-    <main>
-        <!-- Page Wrapper -->
-        <div class="page-wrapper">
+    <body>
+        <main>
+            <!-- Page Wrapper -->
+            <div class="page-wrapper">
 
-            <!-- Post Slider -->
-            <div class="post-slider">
-                <div class="slider-title">
-                    <h1>Nuevas Noticias</h1>
+                <!-- Post Slider -->
+                <div class="post-slider">
+                    <div class="post-slider">
+                        <h1 class="slider-title">Nuevas Noticias</h1>
+                        <i class="fas fa-chevron-left prev"></i>
+                        <i class="fas fa-chevron-right next"></i>
+
+                        <div class="post-wrapper">
+                            
+                            <a href="single.html">
+                                <div class="post">
+                                    <img src="./assets/img/estrategia-economica.jpg" class="slider-image">
+                                    <div class="post-info">
+                                        <h4>Nueva Estrategia Económica en Colima</h4>
+                                        <i class="far fa-user">Nancy Laureano</i>
+                                        &nbsp;
+                                        <i class="far fa-calendar"> Marzo 03, 2025</i>
+                                    </div>
+                                </div>
+                            </a>
+
+                            <a href="single.html">
+                                <div class="post">
+                                    <img src="./assets/img/estrategia-economica.jpg" class="slider-image">
+                                    <div class="post-info">
+                                        <h4>Nueva Estrategia Económica en Colima</h4>
+                                        <i class="far fa-user">Nancy Laureano</i>
+                                        &nbsp;
+                                        <i class="far fa-calendar"> Marzo 03, 2025</i>
+                                    </div>
+                                </div>
+                            </a>
+
+                            <a href="single.html">
+                                <div class="post">
+                                    <img src="./assets/img/estrategia-economica.jpg" class="slider-image">
+                                    <div class="post-info">
+                                        <h4>Nueva Estrategia Económica en Colima</h4>
+                                        <i class="far fa-user">Nancy Laureano</i>
+                                        &nbsp;
+                                        <i class="far fa-calendar"> Marzo 03, 2025</i>
+                                    </div>
+                                </div>
+                            </a>
+
+                            <a href="single.html">
+                                <div class="post">
+                                    <img src="./assets/img/estrategia-economica.jpg" class="slider-image">
+                                    <div class="post-info">
+                                        <h4>Nueva Estrategia Económica en Colima</h4>
+                                        <i class="far fa-user">Nancy Laureano</i>
+                                        &nbsp;
+                                        <i class="far fa-calendar"> Marzo 03, 2025</i>
+                                    </div>
+                                </div>
+                            </a>
+
+                            <a href="single.html">
+                                <div class="post">
+                                    <img src="./assets/img/estrategia-economica.jpg" class="slider-image">
+                                    <div class="post-info">
+                                        <h4>Nueva Estrategia Económica en Colima</h4>
+                                        <i class="far fa-user">Nancy Laureano</i>
+                                        &nbsp;
+                                        <i class="far fa-calendar"> Marzo 03, 2025</i>
+                                    </div>
+                                </div>
+                            </a>
+                            
+                        </div>
+                    </div>
+                    
                 </div>
-
-                <div class="post-wrapper">
-                    <div class ="post">1</div>
-                    <div class ="post">2</div>
-                    <div class ="post">3</div>
-                    <div class ="post">4</div>
-                    <div class ="post">5</div>
-                </div>
+                <!-- //Post Slider -->
 
             </div>
-            <!-- Post Slider -->
-
-        </div>
-        <!-- //Page Wrapper -->
-
-
+            <!-- //Page Wrapper -->
+        </main>
         <!-- jQuery CDN -->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-        <!-- Slick Carousel -->
+        <!-- Custom Script -->
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
-        <!-- Custom Script -->
-         <script src="js/scripts.js"></script>
-    </main>
-</body>
+        <script src="/js/scripts.js"></script>
+        
+    </body>
+</html>
+
