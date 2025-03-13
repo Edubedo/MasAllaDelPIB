37c35cb8ac8c5bc032d7a8fbf7e48871ec4b1a3c
pamela <drodriguez64@col.mx>
2025-03-13 08:58:02 -0600

    agrege el logo como icono en la pagina y modificaciones en el login

--- views/signin.php
+++ views/signin.php
@@ -8,10 +8,9 @@
     <script src="../js/main.js"></script>
     <link rel="icon" href="../assets/img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
     <link rel="stylesheet" href="./css/navbar.css">
-    <link rel="stylesheet" href='./css/login.css'>
-    <link rel="stylesheet" href='./css/footer.css'>
+    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./css/footer.css">
 </head>
 
 <body>
@@ -20,37 +19,37 @@
     <!-- IMPORTAR BARRA DE NAVEGACIÓN -->
 
     <main>
-    <div class="contenedor__todo">
-        <!-- Formularios de login y registro -->
-        <div class="contenedor__Login-register">
-            <form action="" class="formulario__login">
-                <h2><i class="fas fa-user-circle fa-3x"></i></h2>
-                
-                <!-- Contenedor para los iconos y los inputs -->
-                <div class="input-container">
-                    <i class="fas fa-envelope icon fa-2x"></i>
-                    <input type="text" placeholder="Correo Electrónico">
-                </div>
-                
-                <div class="input-container">
-                    <i class="fas fa-lock icon fa-2x"></i>
-                    <input type="password" placeholder="Contraseña">
-                </div>
-                
-                <!-- Enlace para recordar contraseña -->
-                <p><a href="#">¿Olvidaste tu contraseña?</a></p>
-                
-                <button>Entrar</button>
-            </form>
+        <div class="contenedor__todo">
+            <!-- Formularios de login y registro -->
+            <div class="contenedor__Login-register">
+                <!-- Formulario con action para redirigir al login -->
+                <form action="../admin/posts/posts-consulta.php" method="POST" class="formulario__login">
+                    <h2><i class="fas fa-user-circle fa-3x"></i></h2>
+                    
+                    <!-- Contenedor para los iconos y los inputs -->
+                    <div class="input-container">
+                        <i class="fas fa-envelope icon fa-2x"></i>
+                        <input type="text" placeholder="Correo Electrónico" name="email">
+                    </div>
+                    
+                    <div class="input-container">
+                        <i class="fas fa-lock icon fa-2x"></i>
+                        <input type="password" placeholder="Contraseña" name="password">
+                    </div>
+                    
+                    <!-- Enlace para recordar contraseña -->
+                    <p><a href="#">¿Olvidaste tu contraseña?</a></p>
+                    
+                    <!-- Botón de enviar -->
+                    <button type="submit">Entrar</button>
+                </form>
+            </div>
         </div>
-    </div>
-</main>
-
-
-
-   
+    </main>
 
     <!-- IMPORTAR EL FOOTER -->
     <?php include './layout/footer.php'; ?>
     <!-- IMPORTAR EL FOOTER -->
 </body>
+
+</html>
