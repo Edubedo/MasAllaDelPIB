0fa8e8c2cf0273e2b1ab9b496b056383f6f264dc
nanchi1005 <nancy16laureano@gmail.com>
2025-03-13 00:04:32 -0600

    about us responsivo

--- views/about.php
+++ views/about.php
@@ -30,24 +30,25 @@
     </div>
 
     <div class="cuerpo">
-        <div class="info">
-            <p class="texto">
-                Este blog tiene como objetivo informar y crear conciencia sobre la importancia del trabajo
-                decente y el crecimiento económico, destacando su impacto en la sociedad y en el desarrollo
-                sostenible. Buscamos generar un espacio de reflexión y debate en el que las personas puedan
-                conocer más sobre sus derechos laborales, las oportunidades de empleo digno y las acciones
-                necesarias para fomentar una economía inclusiva y equitativa.
-            </p>
-            <p class="texto">
-                Somos un grupo de universitarios apasionados por estos temas, y a través de este proyecto,
-                no solo buscamos aportar información valiosa a nuestra comunidad, sino también adquirir experiencia,
-                desarrollar nuestras habilidades y generar un impacto positivo en la sociedad.
-            </p>
+        <div class="container-nosotros">
+            <div class="info">
+                <p class="texto">
+                    Este blog tiene como objetivo informar y crear conciencia sobre la importancia del trabajo
+                    decente y el crecimiento económico, destacando su impacto en la sociedad y en el desarrollo
+                    sostenible. Buscamos generar un espacio de reflexión y debate en el que las personas puedan
+                    conocer más sobre sus derechos laborales, las oportunidades de empleo digno y las acciones
+                    necesarias para fomentar una economía inclusiva y equitativa.
+                </p>
+                <p class="texto">
+                    Somos un grupo de universitarios apasionados por estos temas, y a través de este proyecto,
+                    no solo buscamos aportar información valiosa a nuestra comunidad, sino también adquirir experiencia,
+                    desarrollar nuestras habilidades y generar un impacto positivo en la sociedad.
+                </p>
+            </div>
+            <div class="foto">
+                <img class="img" src="../assets/img/imagen_nosotros.png" alt="imagen de nosotros">
+            </div>
         </div>
-        <div class="foto">
-            <img class="img" src="../assets/img/imagen_nosotros.png" alt="imagen de nosotros">
-        </div>
-        
 
         <div class="title">
             <h1>COLABORADORES</h1>
