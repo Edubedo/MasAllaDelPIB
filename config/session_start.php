f9304e9a22f8683412eb8ddc96921a0479207da6
Tonatiuh <rpreciado9@ucol.mx>
2025-03-31 10:27:54 -0600

    Se hizo responsivo el buscador de administrador y se arreglo el campo de usuarios de la tabla de crear post

--- config/session_start.php
+++ config/session_start.php
@@ -6,15 +6,18 @@ if (isset($_SESSION['username'])) {
 
     $username = $_SESSION['username'];
 
+    // Consulta SQL simple
     $sql = "SELECT * FROM users WHERE username = '$username'";
     $result = mysqli_query($conexion, $sql);
 
     if ($result && mysqli_num_rows($result) > 0) {
         $usuario = mysqli_fetch_assoc($result);
-        print_r($usuario); // Depuración: Verifica el contenido de $usuario
+
+        // Guardar en la sesión los datos del usuario
         $_SESSION['username'] = $usuario['username'];
         $_SESSION['email'] = $usuario['email'];
         $_SESSION['iduser'] = $usuario['iduser'];
         $_SESSION['id_type_user'] = $usuario['id_type_user'];
     }
 }
+?>
