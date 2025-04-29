3d7358d1e30aa279f784718283d1cc4bb531b2c3
pamela <drodriguez64@col.mx>
2025-04-29 12:42:22 -0600

    icono del logo en todas las rutas

--- admin/posts/editar-perfil.php
+++ admin/posts/editar-perfil.php
@@ -191,6 +191,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar perfil</title>
+
+    <link rel="icon" href="../../assets/img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="css/crear.css">
 </head>
 <body>
