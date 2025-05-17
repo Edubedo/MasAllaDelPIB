e3b7523dee7fca95b7b2d7b4bdb81bdc7258221d
pamela <drodriguez64@col.mx>
2025-05-17 17:35:58 -0600

    ajustes en los enlaces de los correos

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
-    <link rel="stylesheet" href="../../views/css/footer.css">
+    <link rel="stylesheet" href="/views/css/footer.css">
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
@@ -33,20 +33,14 @@
                 <i class="fas fa-envelope icono-correo"></i>
                 <div class="correos-container">
                     <div class="correos">
-                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=nlaureano@ucol.mx"
-                                target="_blank">nlaureano@ucol.mx</a></p>
-                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=ehernandez71@ucol.mx"
-                                target="_blank">ehernandez71@ucol.mx</a></p>
-                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=jangelina@ucol.mx"
-                                target="_blank">jangelina@ucol.mx</a></p>
+                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=nlaureano@ucol.mx" target="_blank">nlaureano@ucol.mx</a></p>
+                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=ehernandez71@ucol.mx" target="_blank">ehernandez71@ucol.mx</a></p>
+                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=jangelina@ucol.mx" target="_blank">jangelina@ucol.mx</a></p>
                     </div>
                     <div class="correos">
-                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=jalcala5@ucol.mx"
-                                target="_blank">jalcala5@ucol.mx</a></p>
-                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=drodriguez64@ucol.mx"
-                                target="_blank">drodriguez64@ucol.mx</a></p>
-                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=jquintero12@ucol.mx"
-                                target="_blank">jquintero12@ucol.mx</a></p>
+                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=jalcala5@ucol.mx" target="_blank">jalcala5@ucol.mx</a></p>
+                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=drodriguez64@ucol.mx" target="_blank">drodriguez64@ucol.mx</a></p>
+                        <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=jquintero12@ucol.mx" target="_blank">jquintero12@ucol.mx</a></p>
                     </div>
                 </div>
             </div>
